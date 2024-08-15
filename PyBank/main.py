d55d972cafb0d8914e8c1a9bbf313f0980ac7b1d
Maura Tobin <mauratobin@Mauras-MacBook-Air.local>
2024-08-15 17:08:55 -0500

    updating files of PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,31 +3,50 @@ import csv
 
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
-# read in the CSV file
+#tracking financial parameters
+total_months = 0
+total_net = 0
+month_of_changes = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 99999999999999]
+net_change_list = []
+
 with open(budget_csv, 'r') as csvfile:
-    # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
+    reader =csv.reader(csvfile)
+    header = next(reader)  # skip header
+    first_row = next(reader)
+    total_months += 1
+    total_net += int(first_row[1])
+    previous_net = int(first_row[1])
 
-def print_
+    for row in reader:
+        total_months += 1
+        total_net += int(row[1])
+        net_change = int(row[1]) - previous_net
+        previous_net = int(row[1])
+        net_change_list.append(net_change)
+        month_of_changes += [row[0]]
+        #calculate greatest increase/decrease
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+    #calculate average net change
+    average_net_change = sum(net_change_list) / len(net_change_list)
+    
+    results = (
+        f"Financial Analysis\n"
+        f"----------------------------\n"
+        f"Total Months: {total_months}\n"
+        f"Total: ${total_net}\n"
+        f"Average Change: ${average_net_change:.2f}\n"
+        f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+        f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})"
+    )
+    print(results)
 
-total_profit_losses = 0
-# The total number of months included in the dataset
-    total_rows = csvfile.readlines()
-    total_months = len(total_rows)
-    print("Total Months:", total_months)
-
-# The net total amount of "Profit/Losses" over the entire period
-    for row in csvreader:
-        try:
-            value = int(row[1])
-            total_profit_losses += value
-        except ValueError:
-            print(f"Skipping non-integer value in row: {row}")
-    print(f"Total: $ {total_profit_losses}")
-
-# The changes in "Profit/Losses" over the entire period, and then the average
-#  of those changes 
-
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire period
+    analysis_path = os.path.join("analysis", "budget_analysis.txt")
+    with open(analysis_path, 'w') as text_file:
+        text_file.write(results)
